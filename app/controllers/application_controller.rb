52aa75d51f110f54a8301dbd4277faafb0b1d997
Elle Faraday <faraday.elle@gmail.com>
2018-05-10 19:20:21 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,7 @@ class ApplicationController < Sinatra::Base
 
   post '/signup' do
       @user = User.new(params)
-      if @user.name && @user.email && @user.save
+      if @user.username && @user.email && @user.save
         session[:id] = @user.id
         redirect "/tweets"
       else
