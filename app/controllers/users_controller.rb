a0abc036272126c433dd43210c9f60fd041d67b5
Elle Faraday <faraday.elle@gmail.com>
2018-05-10 18:14:11 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 
 class UsersController < ApplicationController
-  get '/users/:username' do
-    @user= User.find_by_id(session[:id])
-    erb :'users/show'
-  end
-
   get 'users/logout' do
     session.clear
     flash[:message] = "You have logged out."
     redirect to '/'
+  end
+
+  get '/users/:username' do
+    @user= User.find_by_id(session[:id])
+    erb :'users/show'
   end
 end
