a510c2b2733590599f9ec365393ab7b9f9494d0c
Elle Faraday <faraday.elle@gmail.com>
2018-05-10 18:56:23 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,16 @@ class User < ActiveRecord::Base
   has_secure_password
 
   def slug
-    self.username.
+    slug = self.username.gsub(/[\u0080-\u00ff]/, "").gsub(" ", "-").downcase
+  end
+
+  def self.find_by_slug(slug)
+    found = nil
+    self.all.each do |artist|
+      if artist.slug == slug
+        found = artist
+      end
+    end
+    found
+  end
 end
